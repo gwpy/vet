8b0d99aa7bc4f0757670481c45f2afc4e072e1ec
Duncan Macleod <duncan.macleod@ligo.org>
2014-11-17 12:17:02 -0600

    Metric: whitespace and doc changes only

--- gwpy/toolkits/vet/metric/__init__.py
+++ gwpy/toolkits/vet/metric/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with GWVeto.  If not, see <http://www.gnu.org/licenses/>.
 
-""".. currentmodule:: gwpy.toolkits.vet.metric
+""".. currentmodule:: gwpy.toolkits.vet
 #######
 Metrics
 #######
