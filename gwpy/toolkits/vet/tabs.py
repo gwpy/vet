e3b1165026e6e38346883bd4f3e743d043b00316
Duncan Macleod <duncan.macleod@ligo.org>
2015-04-24 10:31:58 -0500

    vet.tabs: added gwsumm Tab for single flag study
        - can execute veto analysis using gw_summary or gwvet

---
+++ gwpy/toolkits/vet/tabs.py
@@ -0,0 +1,361 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) Duncan Macleod (2014-2015)
+#
+# This file is part of GWpy VET.
+#
+# GWpy VET is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy VET is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy VET.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Summary-page style tabs for GWpy VET
+"""
+
+import os
+
+from gwpy.plotter.tex import label_to_latex
+from gwpy.time import Time
+
+from gwsumm.config import NoOptionError
+from gwsumm.data import get_channel
+from gwsumm.segments import get_segments
+from gwsumm.triggers import (get_triggers, get_etg_table)
+from gwsumm.tabs import get_tab, register_tab
+from gwsumm.html import (data_table, markup)
+from gwsumm.plot import get_plot
+from gwsumm.utils import vprint
+from gwsumm.state import ALLSTATE
+
+from . import version
+from .core import evaluate_flag
+from .plot import *
+from .triggers import re_meta
+from .metric import get_metric
+
+__author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
+__version__ = version.version
+
+
+class FlagTab(get_tab('default')):
+    """Tab for analysing veto performance
+
+    Parameters
+    ----------
+    name : `str`
+        the name of this analysis, either the name of the flag tested,
+        of a descriptive name for the groups of flags
+    start : `float`, `~gwpy.time.LIGOTimeGPS`
+        the GPS start time of the analysis
+    end : `float`, `~gwpy.time.LIGOTimeGPS`
+        the GPS end time of the analysis
+    flags : `list`, optional
+        the list of data-quality flags to be tested. Each flag can be given
+        as a name to be queryied from the segment database, or a 2-tuple of
+        ``(name, sourcefile)``, to be read from file - the ``sourcefile``
+        can then be any file format understood by
+        :meth:`DataQualityFlag.read <gwpy.segments.DataQualityFlag.read>`
+    metrics : `list`, optional
+        the list of performance metrics to use
+    channel : `str`, optional
+        the name of the channel who's triggers are to be used to test
+        event-trigger performance
+    etg : `str`, optional
+        the name of the event-trigger-generator for the above `channel`
+    intersection : `bool`, optional, default: `False`
+        use the intersection of segments from all flags, otherwise use the
+        union
+    **kwargs
+        other keyword arguments to set up this tab get passed to the
+        `DataTab` constructor
+    """
+    type = 'archived-veto-flag'
+
+    def __init__(self, name, start, end,
+                 flags=[],
+                 metrics=[],
+                 channel=None, etg=None,
+                 intersection=False,
+                 plotdir=os.curdir, states=list([ALLSTATE]), **kwargs):
+        if len(flags) == 0:
+            flags = [name]
+            if isinstance(name, tuple):
+                name = name[0]
+        super(FlagTab, self).__init__(
+            name, start, end, states=states, **kwargs)
+        self.flags = list(flags)
+        self.metrics = metrics
+        self.channel = get_channel(channel)
+        self.etg = etg
+        self.intersection = intersection
+        if intersection:
+            self.metaflag = '&'.join(map(str, self.flags))
+        else:
+            self.metaflag = '|'.join(map(str, self.flags))
+        # and set up some plots
+        if not self.plots and not 'plots' in kwargs:
+            self.init_plots(plotdir=plotdir)
+        # make space for the results
+        self.results = {}
+
+    @classmethod
+    def from_ini(cls, config, section, **kwargs):
+        """Parse a new tab from a `ConfigParser` section
+        """
+        # get list of flags
+        kwargs.setdefault(
+            'flags',
+            [f.strip('\n ') for f in config.get(section, 'flags').split(',')])
+        # get list of metrics
+        kwargs.setdefault(
+            'metrics',
+            [get_metric(f.strip('\n ')) for f in
+                 config.get(section, 'metrics').split(',')])
+        # get ETG params
+        try:
+            kwargs.setdefault(
+                'channel', get_channel(config.get(section, 'event-channel')))
+        except NoOptionError:
+            pass
+        else:
+            kwargs.setdefault('etg', config.get(section, 'event-generator'))
+        # get flag combine
+        if len(kwargs['flags']) > 1 and 'intersection' not in kwargs:
+            try:
+                combine = config.get(section, 'combine')
+            except NoOptionError:
+                kwargs['intersection'] = False
+            else:
+                if combine.lower() == 'intersection':
+                    kwargs['intersection'] = True
+                elif combine.lower() != 'union':
+                    raise ValueError("Cannot parse flag combine operator %r" %
+                                     combine)
+                else:
+                    kwargs['intersection'] = False
+        # make tab and return
+        new = super(FlagTab, cls).from_ini(config, section, plots=[], **kwargs)
+        new.init_plots(plotdir=kwargs.get('plotdir', os.curdir))
+        return new
+
+    def init_plots(self, plotdir=os.curdir):
+        """Configure the default list of plots for this tab
+
+        This method configures a veto-trigger glitchgram, histograms of
+        before/after SNR and frequency/mchirp, before and after glitchgrams,
+        and a segment plot.
+
+        This method is a mess, and should be re-written in a better way.
+        """
+        if self.intersection:
+            label = 'Intersection'
+        else:
+            label = 'Union'
+
+        self.layout = (1, 2, 2, 1)
+        before = get_channel(str(self.channel))
+        for state in self.states:
+            if self.channel:
+                after = get_channel('%s-%s%s' % (
+                    str(before), re_meta.sub('-', self.metaflag), str(state)))
+                vetoed = get_channel('%s@%s%s' % (
+                    str(before), re_meta.sub('-', self.metaflag), str(state)))
+                # -- configure trigger plots
+                if 'inspiral' in get_etg_table(self.etg).tableName:
+                    column = 'mchirp'
+                    color = 'new_snr'
+                    logcolor = False
+                    clim = 6,10
+                else:
+                    column = 'peak_frequency'
+                    color = 'snr'
+                    logcolor = True
+                    clim = 3,50
+                histargs = {
+                    'title': 'Impact of %s (%s)' % (
+                        label_to_latex(self.name), self.etg),
+                    'labels': ['Before', 'After'],
+                    'color': ['red', (0.2, 0.8, 0.2)],
+                    'logx': True,
+                    'logy': True,
+                    'histtype': 'stepfilled',
+                    'alpha': 0.6,
+                    'weights': 1/float(abs(self.span)),
+                    'xscale': 'log',
+                    'bins': 100,
+                    'ybound': 1/float(abs(self.span)) * 0.5,
+                    'autoscalex_on': True,
+                    '_tight': True,
+                    'legend-borderaxespad': 0,
+                    'legend-bbox_to_anchor': (1.01, 1),
+                    'legend-loc': 'upper left',
+                }
+                glitchgramargs = {
+                    'etg': self.etg,
+                    'x': 'time',
+                    'y': column,
+                    'logy': True,
+                    'ylabel': get_column_label(column),
+                    'edgecolor': 'none',
+                    'labels': ['_', 'Vetoed'],
+                    'colors': ['lightblue', 'red'],
+                    'autoscaley_on': True,
+                    '_tight': True,
+                    'legend-scatterpoints': 1,
+                    'legend-borderaxespad': 0,
+                    'legend-bbox_to_anchor': (1.01, 1),
+                    'legend-loc': 'upper left',
+                }
+                # plot before/after glitchgram
+                self.plots.append(get_plot('triggers')(
+                    [after, vetoed], self.start, self.end, state=state,
+                    title='Impact of %s (%s)' % (
+                        label_to_latex(self.name), self.etg),
+                    outdir=plotdir, **glitchgramargs))
+
+                # plot SNR histogram
+                p = get_plot('trigger-histogram')(
+                        [before, after], self.start, self.end, state=state,
+                        column='snr', etg=self.etg,
+                        xlabel=get_column_label('snr'),
+                        outdir=plotdir, **histargs)
+                self.plots.append(p)
+                p = get_plot('trigger-histogram')(
+                        [before, after], self.start, self.end, state=state,
+                        column=column, etg=self.etg, outdir=plotdir,
+                        xlabel=get_column_label(column), **histargs)
+                self.plots.append(p)
+
+                # plot triggers before and after
+                glitchgramargs.update({
+                    'color': color,
+                    'clim': clim,
+                    'logcolor': logcolor,
+                    'colorlabel': get_column_label(color),
+                })
+                self.plots.append(get_plot('triggers')(
+                    [before], self.start, self.end, state=state,
+                    outdir=plotdir, **glitchgramargs))
+                self.plots.append(get_plot('triggers')(
+                    [after], self.start, self.end, state=state,
+                    title='After %s (%s)' % (
+                        label_to_latex(self.name), self.etg),
+                    outdir=plotdir, **glitchgramargs))
+
+            # -- configure segment plot
+            segargs = {
+                'state': state,
+                'known': {'hatch': 'x', 'edgecolor': 'lightgray',
+                          'fill': False},
+                'color': 'red',
+            }
+            if len(self.flags) == 1:
+                sp = get_plot('segments')(self.flags, self.start, self.end,
+                                          outdir=plotdir, **segargs)
+            else:
+                sp = get_plot('segments')(
+                    [self.metaflag] + self.flags, self.start, self.end,
+                    labels=([self.intersection and 'Intersection' or 'Union'] +
+                            self.flags), outdir=plotdir, **segargs)
+            self.plots.append(sp)
+
+
+    def process_state(self, state, *args, **kwargs):
+        # first get all of the segments
+        for flag in self.flags:
+            # assume a 2-tuple is a (name, segmentfile) pair
+            if isinstance(flag, (list, tuple)):
+                flag, source = flag
+                get_segments(flag, state, config=kwargs.get('config', None),
+                             cache=source, return_=False)
+        segs = get_segments(self.metaflag, state, kwargs.get('config', None))
+        # then get all of the triggers
+        before = get_triggers(str(self.channel), self.etg, state)
+        # then apply all of the metrics
+        self.results[state] = evaluate_flag(
+            segs, triggers=before, metrics=self.metrics, injections=None,
+            vetotag=str(state))[0]
+        vprint("    Veto evaluation results:\n")
+        for metric, val in self.results[state].iteritems():
+            vprint('        %s: %s\n' % (metric, val))
+        # then pass to super to make the plots
+        return super(FlagTab, self).process_state(state, *args, **kwargs)
+
+    def write_state_html(self, state):
+        # write results table
+        performance = [(str(m), '%.2f %s' % (r.value, r.unit),
+                        m.description.split('\n')[0]) for
+                       (m, r) in self.results[state].iteritems()]
+        pre = markup.page()
+        pre.div(class_='scaffold well')
+        pre.strong('Flag performance summary')
+        pre.add(str(data_table(['Metric', 'Result', 'Description'], performance)))
+        pre.div.close()
+        pre.h2('Figures of Merit')
+        # write configuration table
+        post = markup.page()
+        def add_config_entry(title, entry):
+            post.tr()
+            post.th(title)
+            post.td(entry)
+            post.tr.close()
+        post.h2('Analysis configuration')
+        post.div()
+        post.table(class_='table table-condensed table-hover')
+        add_config_entry('Flags', '<br>'.join(map(str, self.flags)))
+        if len(self.flags) > 1 and self.intersection:
+            add_config_entry('Flag combination', 'Intersection (logical AND)')
+        elif len(self.flags) > 1:
+            add_config_entry('Flag combination', 'Union (logical OR)')
+        add_config_entry('Analysis start time', '%s (%s)' % (
+            str(Time(float(self.start), format='gps', scale='utc').iso),
+            self.start))
+        add_config_entry('Analysis end time', '%s (%s)' % (
+            str(Time(float(self.end), format='gps', scale='utc').iso),
+            self.end))
+        add_config_entry('Event trigger channel', str(self.channel))
+        add_config_entry('Event trigger generator', str(self.etg))
+        post.table.close()
+        post.div.close()
+        post.h2('Segment information')
+        post.div(class_='panel-group', id="accordion")
+        for i, flag in enumerate([self.metaflag] + self.flags):
+            flag = get_segments(flag, state.active, query=False).copy()
+            post.div(class_='panel well panel-primary')
+            post.div(class_='panel-heading')
+            post.a(href='#flag%d' % i, **{'data-toggle': 'collapse',
+                                          'data-parent': '#accordion'})
+            if i == 0:
+                post.h4(self.intersection and 'Intersection' or 'Union',
+                        class_='panel-title')
+            else:
+                post.h4(flag.name, class_='panel-title')
+            post.a.close()
+            post.div.close()
+            post.div(id_='flag%d' % i, class_='panel-collapse collapse')
+            post.div(class_='panel-body')
+            # write segment summary
+            post.p('This flag was defined and had a known state during '
+                   'the following segments:')
+            post.add(self.print_segments(flag.known))
+            # write segment table
+            post.p('This flag was active during the following segments:')
+            post.add(self.print_segments(flag.active))
+
+            post.div.close()
+            post.div.close()
+            post.div.close()
+        post.div.close()
+
+        # then write standard data tab
+        return super(get_tab('default'), self).write_state_html(
+            state, plots=True, pre=pre, post=post)
+
+register_tab(FlagTab)
