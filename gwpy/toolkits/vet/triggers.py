0988f8f419830651e75e6d24d4fc3abb348edca1
Duncan Macleod <duncan.macleod@ligo.org>
2015-04-24 10:27:26 -0500

    vet.triggers: moved the ETG parsing code to gwsumm
        - improved veto method

--- gwpy/toolkits/vet/triggers.py
+++ gwpy/toolkits/vet/triggers.py
@@ -20,147 +20,57 @@
 """
 
 from __future__ import print_function
+import re
 
 from . import version
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
 __version__ = version.version
 
-from glue.lal import Cache
-
-from gwpy.table import lsctables
-from gwpy.table.io import trigfind
 from gwpy.table.utils import get_row_value
-from gwpy.time import to_gps
-from gwpy.segments import (Segment, SegmentList, DataQualityFlag)
+from gwpy.segments import DataQualityFlag
 
-ETG_TABLE = {
-    # single-IFO burst
-    'omicron': lsctables.SnglBurstTable,
-    'omega': lsctables.SnglBurstTable,
-    'omegadq': lsctables.SnglBurstTable,
-    'kleinewelle': lsctables.SnglBurstTable,
-    'kw': lsctables.SnglBurstTable,
-    'dmtomega': lsctables.SnglBurstTable,
-    'dmt_wsearch': lsctables.SnglBurstTable,
-    # multi-IFO burst
-    'cwb': lsctables.MultiBurstTable,
-    # single-IFO inspiral
-    'daily_ihope': lsctables.SnglInspiralTable,
-    'daily_ahope': lsctables.SnglInspiralTable,
-}
+from gwsumm import globalv
+from gwsumm.triggers import get_triggers
+
+re_meta = re.compile('(\||:|\&)')
 
 
-def get_etg_table(etg):
-    """Find which table should be used for the given etg
-
-    Parameters
-    ----------
-    etg : `str`
-        name of Event Trigger Generator for which to query
-
-    Returns
-    -------
-    table : `~gwpy.table.Table`
-        LIGO_LW table registered to the given ETG
-
-    Raises
-    ------
-    KeyError
-        if the ETG is not registered
+def veto(table, flag, tag=''):
+    """Apply a veto to a set of event table and return survivors
     """
-    try:
-        return ETG_TABLE[etg.lower()]
-    except KeyError as e:
-        e.args = ('No LIGO_LW table registered to etg %r' % etg,)
-        raise
-
-
-def register_etg_table(etg, table, force=False):
-    """Register a specific LIGO_LW table to an ETG
-
-    Parameters
-    ----------
-    etg : `str`
-        name of Event Trigger Generator to register
-    table : `~gwpy.table.Table`, `str`
-        `Table` class to register, or the ``tableName`` of the relevant class
-    force : `bool`, optional, default: `False`
-        overwrite an existing registration for the given ETG
-
-    Raises
-    ------
-    KeyError
-        if a `str` table cannot be resolved to a specific class
-    """
-    if isinstance(table, str):
-        try:
-            table = lsctables.TableByName[etg]
-        except KeyError as e:
-            e.args = ('Cannot parse table name %r' % table,)
-    if etg.lower() in ETG_TABLE and not force:
-        raise KeyError('LIGO_LW table already registered to etg %r' % etg,)
-    ETG_TABLE[etg.lower()] = table
-    return table
-
-
-def get_triggers(channel, etg, segments, table=None, columns=None, cache=None):
-    """Fetch a set of event triggers for the given channel
-
-    Parameters
-    ----------
-    channel : `str`, `~gwpy.detector.Channel`
-        name of channel for which to search
-    etg : `str`
-        name of Event Trigger Generator for which to search
-    segments : `~gwpy.segments.DataQualityFlag`, `~gwpy.segments.SegmentList`
-        span over which to read triggers
-    table : `type`, optional
-        table class to read into, e.g. `~gwpy.table.lsctables.SnglBurstTable`
-    columns : `list` of `str`, optional
-        `list` of column names to read, defaults to all valid columns
-    cache : `~glue.lal.Cache`, optional
-        cache of files to use as data source. If not given, the :meth:`fetch`
-        method of the relevant table class will be used to find and read the
-        triggers
-
-    Returns
-    -------
-    table : `~gwpy.table.Table`
-        a LIGO_LW `Table` object, filled with rows
-    """
-    # format segments
-    if isinstance(segments, DataQualityFlag):
-        segments = segments.active
-    elif isinstance(segments, tuple):
-        segments = [Segment(to_gps(segments[0]), to_gps(segments[1]))]
-    segments = SegmentList(segments)
-    # get table class
-    if table is None:
-        table = get_etg_table(etg)
-    elif isinstance(table, str):
-        table = TableByName[table]
-    form = etg
-    # make new table instance
-    new = lsctables.New(table, columns=columns)
-    new.segments = type(segments)()
-    # read new triggers
-    for segment in segments:
-        segment = type(segment)(map(float, segment))
-        filter_ = lambda t: float(get_row_value(t, 'time')) in segment
-        if cache is None:
-            c2 = trigfind.find_trigger_urls(str(channel), etg, segment[0],
-                                            segment[1])
-            form = 'ligolw'
-        else:
-            if isinstance(cache, Cache):
-                c2 = cache.sieve(segment=segment)
+    fname = re_meta.sub('-', flag.name)
+    alabel = '%s-%s%s' % (table.channel, fname, tag)
+    vlabel = '%s@%s%s' % (table.channel, fname, tag)
+    akey = '%s,%s' % (alabel, table.etg.lower())
+    vkey = '%s,%s' % (vlabel, table.etg.lower())
+    if alabel in globalv.TRIGGERS:
+        return get_triggers(alabel, table.etg, flag.known, query=False)
+    else:
+        after = table.copy()
+        vetoed = table.copy()
+        segs = flag.active
+        get = get_row_value
+        for row in table:
+            if float(get(row, 'time')) in segs:
+                vetoed.append(row)
             else:
-                c2 = cache
-            form = etg
-        new.extend(table.read(c2, columns=columns, filt=filter_, format=form))
-        new.segments.append(segment)
+                after.append(row)
+        for t, key in zip([after, vetoed], [akey, vkey]):
+            t.segments = flag.known
+            t.channel = table.channel
+            t.etg = table.etg
+            globalv.TRIGGERS[key] = t
+        return after
 
-    return new
 
-
+def vetoed(table, flag):
+    """Apply a veto to a set of event table and return vetoed events
+    """
+    if isinstance(flag, DataQualityFlag):
+        flag = flag.active
+    after = table.copy()
+    for row in table:
+        if float(get_row_value(row, 'time')) in flag:
+            after.append(row)
+    return after
